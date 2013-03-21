ab9017d6deb938677f3033f10d770a9194ff6524
4ega <4ega89@gmail.com>
2013-03-21 19:05:11 +0300

    Add two forms

--- lc.js
+++ lc.js
@@ -3,11 +3,27 @@ var messages = new Meteor.Collection('messages');
 if (Meteor.isClient) {
   Template.base.userLoggetIn = function() {
     return Meteor.user();
-  }
+  };
+
+  Template.base.creatingAccount = function () {
+    return Session.get('creatingAccount');
+  };
+
+  Template.base.events({
+    'click #toCreateAccountPage': function () {
+      console.log('cap');
+      Session.set('creatingAccount', true);
+      console.log(Session.get('createAccount'));
+    },
+
+    'click #toSignInPage': function () {
+      Session.set('creatingAccount', false);
+    }
+  });
 
   Template.chat.collection = function() {
     return messages.find().fetch().reverse();
-  }
+  };
 
   Template.base.events({
     'click #submit' : function(e, t) {
